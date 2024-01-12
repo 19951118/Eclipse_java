a7400209976982752d11fb38502946b94fbc1a60
re.0 <re.0@localhost>
2024-01-12 16:25:37 +0900

    スッキリjava chap1 end

--- Java_test/src/Java1.java
+++ Java_test/src/Java1.java
@@ -15,5 +15,20 @@ public class Java1 {
 		// 変数の再宣言。データ型は宣言しない。
 		x = 2;
 		System.out.println(y + x);
-	}
+		
+		System.out.println("練習1-1");
+		System.out.println("javaでプログラムを開発するためには、コンパイラと、インタプリタというソフトウェアが必要です。コンパイラは、私達がjavaの文法に沿って記述したソースコードをバイトコードに変換してくれます。インタプリタは内部に持っているJVMの仕組みを使ってこれを解釈し、マシン語に変換してCPUに送り、CPUは命令を実行します。");
+		System.out.println("練習1-2");
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は" + c);
+		System.out.println("練習1-3");
+		boolean result = true;
+		char yoruni = '駆';
+		double pi = 3.14;
+		long number = 314159265853979L;
+		String msg = "ミナトの攻撃！敵に15Pのダメージを与えた！";
+	} 
 }
+
