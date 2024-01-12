ef8379e50f519422b954c5ef749f7e175f56e0c1
re.0 <re.0@localhost>
2024-01-12 13:41:27 +0900

    test commit

---
+++ Java_test/src/Java1.java
@@ -0,0 +1,20 @@
+
+public class Java1 {
+
+	public static void main(String[] args) {
+		//ここからmainメソッド
+		System.out.println("Hello Java!!");
+		System.out.println("+をつけると変数や数列型と併用できる"+ 20);
+		System.out.println("計算もできるが文字列とは併用で来なさそう");
+		System.out.println(10+10);
+		int  x;
+		x = 5;
+		//このような形でまとめられる。finalをつけることで再宣言を防げる。
+		final int y = 3;
+		System.out.println(x * x * 5 + 3);
+		// 変数の再宣言。データ型は宣言しない。
+		x = 2;
+		System.out.println(y + x);
+	}
+
+}
