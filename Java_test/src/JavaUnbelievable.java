94e96e39d3193ca0a8aa54309044722b21d37359
re.0 <reonn1118@yahoo.co.jp>
2024-01-18 17:57:45 +0900

    奇跡体験！アンビリバボー

---
+++ Java_test/src/JavaUnbelievable.java
@@ -0,0 +1,17 @@
+
+public class JavaUnbelievable {
+
+	public static void main(String[] args) {
+		//奇跡体験！アンビリバボー
+		int[] num = new int[12];
+		for(int i = 0; i < num.length; i++) {
+			num[i] = new java.util.Random().nextInt(12);
+		}
+		for(int i = 0; i < num.length; i++) {
+		char[] miracle = {'奇', '跡', '体', '験', '！','ア', 'ン', 'ビ', 'リ', 'バ', 'ボ', 'ー'};
+		System.out.print(miracle[num[i]] + " ");
+		}
+
+	}
+
+}
