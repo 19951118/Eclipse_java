eeac8d6243b10bf0dcc2d705f7e2dbd4dba231b1
re.0 <reonn1118@yahoo.co.jp>
2024-01-27 18:01:45 +0900

    6章終了

--- Java_test/src/java6app/comment/Java6Kouhan.java
+++ Java_test/src/java6app/comment/Java6Kouhan.java
@@ -8,9 +8,8 @@ public class Java6Kouhan {
 	 public static void showMondokoro() throws Exception{
 		 System.out.println("飛車さん、角さん。もういいでしょう。");
 		 System.out.println("この紋所が目にはいらぬか");
+		 //本来java.lang.もつくが、これは常にインポートされていて省略できる
 		 Thread.sleep(3000);
-
-
 		 java6app.comment.Java6Zenhan.doTogame();
 	 }
 }
