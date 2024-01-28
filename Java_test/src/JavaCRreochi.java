207c89375f87b9c624da8661318e0a002c6d9792
re.0 <reonn1118@yahoo.co.jp>
2024-01-28 18:04:04 +0900

    8章2項終了

---
+++ Java_test/src/JavaCRreochi.java
@@ -0,0 +1,28 @@
+
+public class JavaCRreochi {
+
+	public static void main(String[] args) {
+		int win = 0;
+		for (int i = 0;;i++) {
+			int heso = new java.util.Random().nextInt(319);
+			if(win == heso) {
+				System.out.println(i + "回転目");
+				int fever = new java.util.Random().nextInt(2);
+				if(win == fever) {
+					System.out.println("ビッグボーナス！");
+					for(int n = 0; n < 100; n++) {
+						int renchan = new java.util.Random().nextInt(100);
+						if(win == renchan) {
+							System.out.println("an");
+						}
+					}
+				}else {
+					System.out.println("レギュラーボーナス！");
+				}
+				
+				break;
+			}
+		}
+	}
+
+}
