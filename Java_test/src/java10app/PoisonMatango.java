0612bf102ad4912c84deb412c7f2098a66f226b7
re.0 <reonn1118@yahoo.co.jp>
2024-02-26 17:38:53 +0900

    10章終了

--- Java_test/src/java10app/PoisonMatango.java
+++ Java_test/src/java10app/PoisonMatango.java
@@ -9,7 +9,7 @@ public class PoisonMatango extends Matango {
 		super.attack(h);
 		if(poisonAttack > 0) {
 			System.out.println("更に毒の胞子をばらまいた！");
-			int dmg =h.hp / 5;
+			int dmg = h.hp / 5;
 			h.hp -= dmg;
 			System.out.println(dmg + "ポイントのダメージ！");
 			this.poisonAttack--;
