66f12739f3ce3bce8025c5408e71b257c0b08c7b
re.0 <reonn1118@yahoo.co.jp>
2024-03-12 23:11:13 +0900

    出玉追加、連チャン数追加

--- Java_test/src/JavaCRreochi.java
+++ Java_test/src/JavaCRreochi.java
@@ -1,28 +1,34 @@
 
 public class JavaCRreochi {
-//CRれおっち
+//CRおもっち1/319 
 	public static void main(String[] args) {
+		int dedama = 0;
 		int win = 0;
 		for (int i = 0;;i++) {
 			int heso = new java.util.Random().nextInt(319);
 			if(win == heso) {
 				System.out.println(i + "回転目");
+				//大当たり振り分け抽選
 				int fever = new java.util.Random().nextInt(2);
 				if(win == fever) {
-					System.out.println("ビッグボーナス！");
-					for(int n = 0; n < 100; n++) {
-						int renchan = new java.util.Random().nextInt(100);
-						if(win == renchan) {
-							System.out.println("an");
+					int renchan = 1;
+					dedama += 1500;
+					System.out.println("ビッグボーナス！\n 出玉"+ dedama + "発");
+					//フィーバーモード1/100を100回
+					for(int n = 0; n < 144; n++) {
+						int migiuti = new java.util.Random().nextInt(100);
+						if(win == migiuti) {
+							dedama += 1500;
+							renchan ++;
+							System.out.println(n +"回転目" + renchan + "連ボーナス！\n 出玉"+ dedama + "発");
+							n = 0;
 						}
 					}
 				}else {
-					System.out.println("レギュラーボーナス！");
-				}
-				
+					System.out.println("丸焦げボーナス！1500");
+				}				
 				break;
 			}
 		}
 	}
-
 }
