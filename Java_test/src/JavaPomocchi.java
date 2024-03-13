d318287320fc65f897cf3da63b91a87dbf85cf12
re.0 <reonn1118@yahoo.co.jp>
2024-03-13 17:19:55 +0900

    軍資金、回転数入力機能追加

--- Java_test/src/JavaPomocchi.java
+++ Java_test/src/JavaPomocchi.java
@@ -2,51 +2,70 @@
 public class JavaPomocchi {
 //P　javaから始めるおもち生活　鬼がかりVer.1/319 
 	public static void main(String[] args) {
-		int dedama = 0;
-		int win = 0;
-		for (int i = 0;;i++) {
-			int heso = new java.util.Random().nextInt(319);
-			if(win == heso) {
-				System.out.println(i + "回転目");
-				//大当たり振り分け抽選55%
-				int fever = new java.util.Random().nextInt(100);
-				if(44 < fever) {
-					int renchan = 1;
-					dedama += 3000;
-					System.out.println("鬼がかり3000！\n 出玉"+ dedama + "発");
-					//フィーバーモード1/100を144回
-					for(int n = 0; n < 144; n++) {
-						int migiuti = new java.util.Random().nextInt(100);
-						if(win == migiuti) {
-							//大当たり振り分け
-							int huriwake = new java.util.Random().nextInt(100);
-							if(huriwake < 55) {
-								dedama += 1500;
-								renchan ++;
-								System.out.println(n +"回転目" + renchan + "連ボーナス！\n おもちボーナス1500！出玉計"+ dedama + "発");
-								n = 0;
-							}else if(huriwake > 54 && huriwake < 81) {
-								dedama += 3000;
-								renchan ++;
-								System.out.println(n +"回転目" + renchan + "連ボーナス！\n 鬼がかり3000!出玉計"+ dedama + "発");
-								n = 0;
-							}else if(huriwake > 80 && huriwake < 95) {
-								renchan ++;
-								System.out.println(n +"回転目" + renchan + "連ボーナス！\n リスタート!出玉計"+ dedama + "発");
-								n = 0;
-							}else {
-								dedama += 300;
-								renchan ++;
-								System.out.println(n +"回転目" + renchan + "連ボーナス！\n ボーナス300!出玉計"+ dedama + "発");
-								n = 0;
+		System.out.println("軍資金を入力してください");
+		int money = new java.util.Scanner(System.in).nextInt();
+		if(money < 500) {
+			System.out.println("お金を稼いでからまた来てね！");
+		}else {
+			System.out.println("500円辺り何回転の台？");
+			int kaiten = new java.util.Scanner(System.in).nextInt();
+			int dedama = 0;
+			int win = 0;
+			money -= 500;
+			for (int i = 0;;i++) {
+				if(money < 500) {
+					System.out.println("軍資金がなくなりました。。。");
+					break;
+				}else if(i % kaiten == 0){
+					money -= 500;
+				}
+				int heso = new java.util.Random().nextInt(319);
+				if(win == heso) {
+					System.out.println(i + "回転目。残金" + money);
+					//大当たり振り分け抽選55%
+					int fever = new java.util.Random().nextInt(100);
+					if(44 < fever) {
+						int renchan = 1;
+						dedama += 3000;
+						System.out.println("鬼がかり3000！\n 出玉"+ dedama + "発");
+						//フィーバーモード1/100を144回
+						for(int n = 0; n < 144; n++) {
+							int migiuti = new java.util.Random().nextInt(100);
+							if(win == migiuti) {
+								//大当たり振り分け
+								int huriwake = new java.util.Random().nextInt(100);
+								if(huriwake < 55) {
+									dedama += 1500;
+									renchan ++;
+									System.out.println(n +"回転目" + renchan + "連ボーナス！\n おもちボーナス1500！出玉計"+ dedama + "発");
+									n = 0;
+								}else if(huriwake > 54 && huriwake < 81) {
+									dedama += 3000;
+									renchan ++;
+									System.out.println(n +"回転目" + renchan + "連ボーナス！\n 鬼がかり3000!出玉計"+ dedama + "発");
+									n = 0;
+								}else if(huriwake > 80 && huriwake < 95) {
+									renchan ++;
+									System.out.println(n +"回転目" + renchan + "連ボーナス！\n リスタート!出玉計"+ dedama + "発");
+									n = 0;
+								}else {
+									dedama += 300;
+									renchan ++;
+									System.out.println(n +"回転目" + renchan + "連ボーナス！\n ボーナス300!出玉計"+ dedama + "発");
+									n = 0;
+								}
+								
 							}
-							
 						}
-					}
-				}else {
-					System.out.println("通常ボーナス！1500");
-				}				
-				break;
+					}else {
+						dedama += 1500;
+						System.out.println("通常ボーナス！1500");
+					}	
+					System.out.println("結果" + dedama + "発獲得！換金します");
+					money += dedama * 4;
+					System.out.println("こづかいが" + money + "円になった！");
+					break;
+				}
 			}
 		}
 	}
