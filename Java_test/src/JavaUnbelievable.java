0318b614513260c08015d41a58e68f3ac7261854
re.0 <reonn1118@yahoo.co.jp>
2024-04-07 10:54:02 +0900

    全然短くできた。

--- Java_test/src/JavaUnbelievable.java
+++ Java_test/src/JavaUnbelievable.java
@@ -3,13 +3,10 @@ public class JavaUnbelievable {
 
 	public static void main(String[] args) {
 		//奇跡体験！アンビリバボー
-		int[] num = new int[12];
-		for(int i = 0; i < num.length; i++) {
-			num[i] = new java.util.Random().nextInt(12);
-		}
-		for(int value : num) {
 		char[] miracle = {'奇', '跡', '体', '験', '！','ア', 'ン', 'ビ', 'リ', 'バ', 'ボ', 'ー'};
-		System.out.print(miracle[value] + " ");
+		for(int i = 0; i < miracle.length; i++) {
+			int num = new java.util.Random().nextInt(12);
+			System.out.print(miracle[num]);
 		}
 
 	}
